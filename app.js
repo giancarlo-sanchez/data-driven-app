49812b8e0093950c3e75995ab22c90cb22bb9446
jank_ <jank_329@hotmail.com>
2020-05-07 13:06:22 -0500

    creating app.js file that has an express server

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const routes = require('./routes.js');
+const app = express();
+
+app.set('view engine','pug');
+app.use(routes);
+
+const port = 8080;
+app.listen(port,()=>console.log(`Listening on port ${port}...`))
