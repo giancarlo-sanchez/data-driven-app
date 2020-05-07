47f1aea088161e2a653438c028232baf30fd1277
jank_ <jank_329@hotmail.com>
2020-05-07 16:44:07 -0500

    Creating new configuration for the server scripts

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const morgan = require('morgan')
+
 const routes = require('./routes.js');
+
 const app = express();
 
+app.use(morgan('dev'));
 app.set('view engine','pug');
 app.use(routes);
 
-const port = 8080;
-app.listen(port,()=>console.log(`Listening on port ${port}...`))
+module.exports = app;
+
+// const port = 8080;
+// app.listen(port,()=>console.log(`Listening on port ${port}...`))
