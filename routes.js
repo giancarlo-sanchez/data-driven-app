76ec449ddcd49c3fb5a863498fa3338a9ce3c586
jank_ <jank_329@hotmail.com>
2020-05-07 19:41:23 -0500

    Adding an asynchronous route handler wrapper function

--- routes.js
+++ routes.js
@@ -1,14 +1,14 @@
 const express = require('express');
-const router = express.Router();
+
 const db = require('./db/models');
 
-router.get('/', async (req, res, next) => {
-    try {
-      const books = await db.Book.findAll({ order: [['title', 'ASC']] });
-      res.render('book-list', { title: 'Books', books });
-    } catch (err) {
-      next(err);
-    }
-  });
+const router = express.Router();
+
+const asyncHandler = (handler) => (req, res, next) => handler(req, res, next).catch(next);
+
+router.get('/', asyncHandler(async (req, res) => {
+ const books = await db.Book.findAll({ order: [['title', 'ASC']] });
+ res.render('book-list', { title: 'Books', books });
+}));
 
 module.exports = router;
